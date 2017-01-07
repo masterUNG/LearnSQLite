bb11f1d6d61dba8d72017bb6f6ea36ad7eb57a76
masterUNG <phrombutr@gmail.com>
2017-01-07 14:15:31 +0700

    Bind Finish

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -2,10 +2,12 @@ package appewtc.masterung.learnsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
+    private Button signInButton, singUpButton;
 
 
 
@@ -13,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+
+
     }   // Main Method นี่คือเมธอดหลัก
+
+    //คือการผูกความสัมพันระหว่าง ตัวแปร และ ออปเจค บน Activity
+    private void bindWidget() {
+
+
+
+    }
 
 }   // Main Class
