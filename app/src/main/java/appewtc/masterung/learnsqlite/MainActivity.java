8271c82aa6263ad95c3f1be0f80a0eb2814a2044
masterUNG <phrombutr@gmail.com>
2017-01-07 13:04:16 +0700

    After Lunch

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   // Main Method นี่คือเมธอดหลัก
+
+}   // Main Class
