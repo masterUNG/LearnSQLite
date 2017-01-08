3c93aa383af536d226d7a7eda5c21157470a5c7c
masterUNG <phrombutr@gmail.com>
2017-01-08 13:53:02 +0700

    Check Space OK

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private Button signInButton, singUpButton;
     private MyManage myManage;
-
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
@@ -37,14 +40,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //For singIn
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAuthen();
+            }
+        });
+
 
     }   // buttonController
+
+    private void checkAuthen() {
+
+        //Get Value
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            myDialog(getResources().getString(R.string.haveSpace));
+        }
+
+
+    }   // checkAuthen
+
+    private void myDialog(String string) {
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+    }
 
     //คือการผูกความสัมพันระหว่าง ตัวแปร และ ออปเจค บน Activity
     private void bindWidget() {
 
         signInButton = (Button) findViewById(R.id.button);
         singUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
 
     }
 
